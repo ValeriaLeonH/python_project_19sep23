78a71039fdd5c50468adc5cbd8efc81161f19a30
ValeriaLeonH <valerialeon837@gmail.com>
2023-09-19 10:48:15 -0400

    feat: save word guessing game

---
+++ word_guessing_game.py
@@ -0,0 +1,67 @@
+import random 
+
+# Library that we use in order to choose
+# bn random words from a list of words
+
+name = input("What's your name?")
+
+# Here the user is asked to  enter the name first
+print("Good Luck !", name)
+words = ['rainbow', 'computer', 'science', 'programming', 'python', 'mathematics', 'player', 'condition', 'reverse', 'water', 'board', 'geeks']
+
+#Function will choose one random 
+# word from this list of words
+word = random.choice(words)
+
+print("Guess the characters")
+guesses = ''
+
+#any number of turns can be used here
+turns = 12
+while turns > 0: 
+  # counts the number of times a user fails
+  failed = 0
+  
+  #all characters from the input
+  #work taking one at time 
+  for char in word :
+    #comparing the character with
+    #the character in guesses 
+    if char in guesses: 
+      print(char, end=" ")
+    else: 
+      print("_")
+      # for every failure 1 will be 
+      # incremented in failure 
+      failed += 1 
+  if failed == 0:
+    # User will win the game if failure is 0
+    # and 'You Win' will be given as output 
+    print("You Win")
+    
+    #this print the correct word
+    print("The word is: ", word)
+    break
+  
+  #if user has input the wrong alphabet then 
+  # if will ask use to enter another alphabet 
+  print()
+  guess = input("Guess a character: ")
+
+  # Every input with the character in wod
+  guesses += guess 
+  
+  #check input with the character in word 
+  if guess not in word:
+    turns -= 1
+    
+    # if the character doesn't match the word
+    # then "wrong" will be given as output 
+    print("Wrong")
+    
+    # this will print the number of 
+    # turns left for the user
+    print("You have", + turns, 'more guesses')
+    if turns == 0:
+      print("You Loose")   
+  
